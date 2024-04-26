df9dd51730f0a04ab99ec04f0edf817fdf307e7c
Mark Bird <m_bird2@mail.fhsu.edu>
2024-04-26 14:33:37 -0700

    U

--- controller/statesController.js
+++ controller/statesController.js
@@ -1,2 +1,14 @@
+const States = require('../model/States');
 const states = require('../model/States');
+const allStatesData = require('../model/statesData.json');
+
+const getAllStates = async (req, res) => {
+
+    const states = await States.find();
+    if(!states) return res.status(204).json({ 'message' : 'No states found.'});
+    res.json(states);
+
+    
+}
+
 
--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const path = require('path');
 const statesData = require('./model/statesData.json');
 const cors = require('cors');
 const PORT = process.env.PORT || 3500;
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false}));
 
 app.use(express.json());
 
-app.use(cors());
+app.get('/', (req, res) => {
 
+res.sendFile(path.join(__dirname, 'views', 'index.html'));
+
+})
 
 app.get('/states/', (req, res) => {
 
@@ -25,12 +29,23 @@ res.json(statesData);
 
 
 
-app.get('/*', (req, res) => {
+app.all('*', (req, res) => {
+
+res.status(404);
+if(req.accepts('html')) {
+
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+} else if (req.accepts('json')){
+    res.json({"error" : "404 Not Found"});
+
+} else {
+    res.type('txt').send('404 Not Found');
+}
 
 
-res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 
-})
+
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
