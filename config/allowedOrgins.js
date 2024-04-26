c929908a8ea3b0a8aac4f3d3d9859dc8dc9bce81
Mark Bird <m_bird2@mail.fhsu.edu>
2024-04-26 13:19:17 -0700

    u

--- config/allowedOrgins.js
+++ config/allowedOrgins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'https://dazzling-snickerdoodle-777101.netlify.app/',
+    'https://glitch.com/edit/#!/handsomely-cloudy-freedom',
 ];
 
 module.exports = allowedOrigins;
